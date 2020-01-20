f325bc85f52efb3e75c8dd99ee2470e1c94d5bac
Salomon KABONGO <salomon@aims.ac.za>
2020-01-20 00:27:02 +0000

    uploaded the unigram, attempt

---
+++ Speech and Language Processing_Exercise_Solutions/c3_q3_8.py
@@ -0,0 +1,17 @@
+from collections import Counter
+import utils
+
+# get list of words
+
+def question38(Text):
+    words = Text.split()
+    word_counts = Counter(words)
+    sorted_vocab = sorted(word_counts, key=word_counts.get, reverse=True)
+    # create int_to_vocab dictionaries
+    unigram = {word: (word_counts[word]+1)/(len(words)+len(set(sorted_vocab))) for word in sorted_vocab}
+
+    return unigram
+
+Text = "The workplace has changed: as technology continues to rapidly transform industries and jobs, staying relevant and competitive requires continuously building completely new skill sets. But we are here to help you adapt to the demands of the modern workplace and the lifestyle of the modern worker. Read up on what modular education is and how it can benefit you!"
+print(question38(Text))
+
