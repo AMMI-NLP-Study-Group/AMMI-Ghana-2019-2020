6f468749359b4f91a66a2e909b25a3f664089f2c
Salomon KABONGO <salomon@aims.ac.za>
2020-01-21 11:20:18 +0000

    Changing question 1 to handle question 3.9

--- Speech and Language Processing_Exercise_Solutions/c3_q3_8.py
+++ Speech and Language Processing_Exercise_Solutions/c3_q3_8.py
@@ -5,8 +5,8 @@ def question38(Text):
     words = Text.split() 
     words = [word.lower() for word in words]
     word_counts = Counter(words)
-    sorted_vocab = sorted(word_counts, key=word_counts.get, reverse=True)
-    uni_bi_gram['unigrams'] = {word: word_counts[word] for word in sorted_vocab}
+    popular_unigram_sorted = sorted(word_counts, key=word_counts.get, reverse=True)
+    uni_bi_gram['unigrams'] = {word: word_counts[word] for word in popular_unigram_sorted}
     temp_bigram = {}
     for i in range(0,len(words) -1):
         bigram  = " ".join(words[i:i+2] )
@@ -14,13 +14,14 @@ def question38(Text):
             temp_bigram[bigram] = 1
         else:
             temp_bigram[bigram] += + 1
+    popular_temp_bigram = sorted(temp_bigram, key = temp_bigram.get, reverse = True)
     uni_bi_gram['trigrams'] = temp_bigram
-    return uni_bi_gram
+    return uni_bi_gram, popular_unigram_sorted[:10], popular_temp_bigram[:10]
 
 Text = "The workplace has changed: as technology continues to rapidly \
     transform industries and jobs, staying relevant and competitive requires \
     continuously building completely new skill sets. But we are here to help you \
     adapt to the demands of the modern workplace and the lifestyle of the modern worker. \
     Read up on what modular education is and how it can benefit you!"
-print(question38(Text))
+print(question38(Text)[0]) # 0 to print only the dictionary of all the unigram and bigrams
 
---
+++ Speech and Language Processing_Exercise_Solutions/c3_q3_9.py
@@ -0,0 +1,13 @@
+from c3_q3_8 import question38
+
+
+Text = "The workplace has changed: as technology continues to rapidly \
+    transform industries and jobs, staying relevant and competitive requires \
+    continuously building completely new skill sets. But we are here to help you \
+    adapt to the demands of the modern workplace and the lifestyle of the modern worker. \
+    Read up on what modular education is and how it can benefit you!"
+print(question38(Text)[1]) # 1 to print 10 most common unigrams
+print(question38(Text)[2]) # 2 to print 10 most common bigrams
+
+
+
