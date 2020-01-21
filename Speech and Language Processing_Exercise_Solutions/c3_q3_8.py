91c5d2cc9ec9707663e6090abf1f7f99468cf9ac
Salomon KABONGO <salomon@aims.ac.za>
2020-01-21 11:07:30 +0000

    First stable version of question 3_8

--- Speech and Language Processing_Exercise_Solutions/c3_q3_8.py
+++ Speech and Language Processing_Exercise_Solutions/c3_q3_8.py
@@ -1,14 +1,26 @@
 from collections import Counter
 
 def question38(Text):
-    words = Text.split()
+    uni_bi_gram = {'unigrams': dict(), 'trigrams':dict()}
+    words = Text.split() 
+    words = [word.lower() for word in words]
     word_counts = Counter(words)
     sorted_vocab = sorted(word_counts, key=word_counts.get, reverse=True)
-    # create int_to_vocab dictionaries
-    unigram = {word: (word_counts[word]+1)/(len(words)+len(set(sorted_vocab))) for word in sorted_vocab}
+    uni_bi_gram['unigrams'] = {word: word_counts[word] for word in sorted_vocab}
+    temp_bigram = {}
+    for i in range(0,len(words) -1):
+        bigram  = " ".join(words[i:i+2] )
+        if not bigram  in temp_bigram:
+            temp_bigram[bigram] = 1
+        else:
+            temp_bigram[bigram] += + 1
+    uni_bi_gram['trigrams'] = temp_bigram
+    return uni_bi_gram
 
-    return unigram
-
-Text = "The workplace has changed: as technology continues to rapidly transform industries and jobs, staying relevant and competitive requires continuously building completely new skill sets. But we are here to help you adapt to the demands of the modern workplace and the lifestyle of the modern worker. Read up on what modular education is and how it can benefit you!"
+Text = "The workplace has changed: as technology continues to rapidly \
+    transform industries and jobs, staying relevant and competitive requires \
+    continuously building completely new skill sets. But we are here to help you \
+    adapt to the demands of the modern workplace and the lifestyle of the modern worker. \
+    Read up on what modular education is and how it can benefit you!"
 print(question38(Text))
 
