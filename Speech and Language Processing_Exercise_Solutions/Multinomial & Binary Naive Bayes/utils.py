6e6710772f721c455f5f1982c7361d04e18ec801
Salomon KABONGO <salomon@aims.ac.za>
2020-02-09 18:50:26 +0000

    regular expression

---
+++ Speech and Language Processing_Exercise_Solutions/Multinomial & Binary Naive Bayes/utils.py
@@ -0,0 +1,9 @@
+from typing import Set
+import re
+
+def tokenize(text: str) -> Set[str]:
+    text = text.lower()                         # Convert to lowercase,
+    all_words = re.findall("[a-z0-9']+", text)  # extract the words, and
+    return set(all_words)                       # remove duplicates.
+
+assert tokenize("Data Science 01, is & _ is science") == {"data", "science", "is"}
